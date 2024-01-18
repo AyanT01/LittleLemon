2992faabd73119dd9a58cd331ae427ba20698458
ayant <134538616+AyanT01@users.noreply.github.com>
2024-01-18 09:38:29 -0500

    Configure manager operations and limiting access of various api requests using authorization

--- littlelemon/LittleLemon_API/views.py
+++ littlelemon/LittleLemon_API/views.py
@@ -42,19 +42,71 @@ def menu_items(request):
 @api_view(["GET","POST","PUT","PATCH","DELETE"]) 
 @permission_classes([IsAuthenticated])
 def managers(request):
-    if request.method == "GET":
-        UserModel = get_user_model().objects
-        serialized_item = UserSerializer(UserModel,many=True) 
-        return Response(serialized_item.data)
+    if request.user.groups.filter(name="Manager").exists():
+        UserModel = get_user_model().objects.all()
+        Manager = get_user_model().objects.filter(groups__name='Manager')
+        if request.method == "GET":
+            serialized_item = UserSerializer(UserModel,many=True) 
+            return Response(serialized_item.data)
+        username = request.data["username"]
+        if username:
+            user = get_object_or_404(UserModel, username=username)
+            #managers = UserModel.objects.get(name='Manager')
+            if request.method == 'POST':
+                Manager.user_set.add(user)
+            elif request.method == 'DELETE':
+                Manager.user_set.remove(user)
+            return Response({"message": 'ok'})
+    else:
+        return Response({'message': 'error'})
         
-@api_view()
+        
+@api_view(["GET","POST","PUT","PATCH","DELETE"])
 def single_menu_item(request,pk):
     if request.method == "GET":
         item = get_object_or_404(Menu,id=pk)
         serialized_item = MenuItemSerializer(item)
         return Response(serialized_item.data)    
-        
+    if request.user.groups.filter(name="Manager").exists():
+        if request.method == "POST":
+            serialized_item = MenuItemSerializer(data=request.data)
+            serialized_item.is_valid(raise_exception=True) 
+            serialized_item.save() 
+            return Response(serialized_item.data) 
+            pass 
+        if request.method == "DELETE":
+            Menu.objects.filter(id=pk).delete()
+            return Response({"message":"Deletion successful"})
+        if request.method == "PUT":
+            item = Menu.objects.get(pk=pk)
+            item_data = MenuItemSerializer(instance=item, data=request.data)
+            item_data.is_valid(raise_exception=True) 
+            item_data.save() 
+            return Response(item_data.data)
+        if request.method == "PATCH":
+            item = Menu.objects.get(id=pk) 
+            item_data = MenuItemSerializer(instance=item,data=request.data,partial=True)
+            item_data.is_valid(raise_exception=True) 
+            item_data.save() 
+            return Response(item_data.data)
+            pass
+    else:
+        return Response({"message":"You are not authorized"})
 
 class MenuItemView(generics.ListAPIView):
     queryset = Menu.objects.all()
     serializer_class = MenuItemSerializer
+
+"""
+
+        if request.method == "POST":
+            serialized_item = UserSerializer(data=request.data)
+            serialized_item.is_valid(raise_exception=True) 
+            serialized_item.save() 
+            return Response(serialized_item.data) 
+            pass
+    
+    else:
+        return Response({"message":"You are not authorized"})
+
+"""
